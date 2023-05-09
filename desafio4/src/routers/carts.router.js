023aa5b854c2cdbf77008fbf35b209ab8bf96fc3
NataliaChiara <natalia.chiara.98@gmail.com>
2023-05-09 11:05:07 -0300

    post

--- desafio4/src/routers/carts.router.js
+++ desafio4/src/routers/carts.router.js
@@ -2,42 +2,80 @@ import { Router } from "express";
 
 const cartsRouter = Router();
 
-let appId = 2;
+let appId = 3;
 
 let carts = [
-  { id: 1, products: ["a", "b", "c"] },
-  { id: 2, products: ["e", "f", "g"] },
+  {
+    id: 1,
+    products: [
+      { id: 1, quantity: 1 },
+      { id: 2, quantity: 2 },
+    ],
+  },
+  {
+    id: 2,
+    products: [
+      { id: 3, quantity: 3 },
+      { id: 4, quantity: 4 },
+    ],
+  },
 ];
 
-// cartsRouter.post("/", (req, res) => {
-//   let allCarts = carts;
-//   let products = req.body;
-//   let array = Array.from(products);
-//   let arrayLenght = array.length;
+cartsRouter.get("/:cid", (req, res) => {
+  let allcarts = carts;
+  let idToFind = Number(req.params.cid);
+  let cartIndex = allcarts.findIndex((cart) => cart.id === idToFind);
+  if (cartIndex === -1) {
+    res.send("No existe");
+  } else {
+    let cartFound = allcarts[cartIndex];
+    res.send(cartFound);
+  }
+});
 
-//   if (arrayLenght > 0) {
-//     let cart = {
-//       id: appId,
-//       products: [products],
-//     };
-//     appId++;
-//     allCarts.push(cart);
-//     res.status(201).send(cart);
-//   } else {
-//     res.status(400).send("No se agrego");
-//   }
-// }); FUNCIONA
+cartsRouter.post("/", (req, res) => {
+  let allCarts = carts;
+  let newCart = {
+    id: appId,
+    products: [],
+  };
+  appId++;
+  allCarts.push(newCart);
+  res.status(201).send(newCart);
+});
 
-// cartsRouter.get("/:cid", (req, res) => {
-//   let idToFind = Number(req.params.cid);
-//   let allcarts = carts;
-//   let cartIndex = allcarts.findIndex((cart) => cart.id === idToFind);
-//   if (cartIndex === -1) {
-//     res.send("No existe");
-//   } else {
-//     let cartFound = allcarts[cartIndex];
-//     res.send(cartFound);
-//   }
-// }); FUNCIONA NO TOQUES MAS, DROGADA
+cartsRouter.post("/:cid/product/:pid", (req, res) => {
+  let allCarts = carts;
+  let cartToFind = Number(req.params.cid);
+  let cartIndex = allCarts.findIndex((cart) => cart.id === cartToFind);
+
+  if (cartIndex === -1) {
+    res.send("No existe ese id");
+  } else {
+    let cartFound = allCarts[cartIndex];
+
+    let allProducts = cartFound.products;
+    let productToFind = Number(req.params.pid);
+    let productIndex = allProducts.findIndex(
+      (product) => product.id === productToFind
+    );
+
+    if (productIndex === -1) {
+      res.status(400).send("No existe ese producto");
+    } else {
+      let productFound = allProducts[productIndex];
+
+      let newQuantity = req.body.quantity;
+      productFound = {
+        ...productFound,
+        quantity: productFound.quantity + newQuantity,
+      };
+
+      res.status(201).send(productFound);
+    }
+  }
+});
+
+// productFound > allProducts > cartFound > allCarts
 
 export { cartsRouter };
